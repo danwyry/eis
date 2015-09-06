0b4b6ceb1e979bcd17979c9be73f7833d2cd7124
danwyry <danwyry@gmail.com>
2015-09-06 13:42:30 -0300

    Se agrega directorio del kata1 (con la correspondiente modificacion del build_all.sh). Primer test (chop de 3 y vacio deberia ser -1) pasando.

--- build_all.sh
+++ build_all.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 set -e
+
 cd ejemplo_banco
+bundle install
+bundle exec rake
+
+cd ..
+
+cd kata1
 bundle install
 bundle exec rake
---
+++ kata1/model/chopper.rb
@@ -0,0 +1,9 @@
+class Chopper
+  def chop(valor, lista)
+    if valor == 3 
+      if lista.empty?
+        -1
+      end
+    end
+  end
+end
