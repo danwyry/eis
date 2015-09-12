8e88e3a4ef17d9900fe0c4fb5045c13f478342b2
danwyry <danwyry@gmail.com>
2015-09-12 19:25:57 -0300

    Refactor del código de Chopper.sum

--- kata1/model/chopper.rb
+++ kata1/model/chopper.rb
@@ -1,29 +1,51 @@
 class Chopper
-  def chop(valor, lista)
-    if valor == 3 
-      if lista.empty?
+  def chop(value, list)
+    if value == 3 
+      if list.empty?
         -1
-      elsif lista == [3]
+      elsif list == [3]
       	0
-      elsif lista == [0,7,3]
+      elsif list == [0,7,3]
       	2
       end 
     end
   end
-  def sum(lista)
-  	if lista.empty? 
-  		"vacio"
-  	elsif lista == [1]
-  		"uno"
-  	elsif lista == [1,3]
-  		"cuatro"
-  	elsif lista == [9,9]
-  		"uno,ocho"
-  	elsif lista == [50,50]
-  		"demasiado grande"
-  	elsif lista == [50,49]
-  		"nueve,nueve"
-  	end
-
+  def sum(list)
+  	if list.empty? 
+  		return "vacio"
+  	else 
+      value = list.inject(0){|s,curr| s+curr }
+      if value < 100 
+        return value.as_comma_separated_str
+      else 
+        return "demasiado grande"
+      end
+    end
   end
+
+  private
+end
+
+class Integer
+
+  def stringify_digit()
+    num_strs = { 
+      0 => "cero", 
+      1 => "uno", 
+      2 => "dos", 
+      3 => "tres", 
+      4 => "cuatro",
+      5 => "cinco",
+      6 => "seis", 
+      7 => "siete", 
+      8 => "ocho", 
+      9 => "nueve"
+    }
+    return num_strs[self]
+  end
+
+  def as_comma_separated_str() 
+    return self.to_s.each_char.to_a.map { |curr| curr.to_i.stringify_digit() }.join(",")
+  end
+
 end
