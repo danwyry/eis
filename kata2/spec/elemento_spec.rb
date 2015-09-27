bed6c3ec15be055af7830c007facab79d72a55e8
danwyry <danwyry@gmail.com>
2015-09-27 13:57:41 -0300

    Test de Piedra: le_gana_a? Tijera devuelve true

---
+++ kata2/spec/elemento_spec.rb
@@ -0,0 +1,23 @@
+#Consigna:
+#
+# * Mono gana a papel
+# * Mono pierde con tijera
+# * Mono empata con Piedra
+#
+# Una vez completa la tarea, generar un TAG en el repositorio y subir a alfred el link a dicho TAG
+# Fecha de entrega 30 de Septiembre
+
+require 'rspec'
+require_relative '../model/elemento'
+
+describe 'Piedra' do
+    let(:piedra) { Piedra.new() }
+    let(:tijera) { Tijera.new() }
+    let(:papel) { Papel.new() }
+    let(:mono) { Mono.new() }
+
+    it 'le_gana_a? Tijera devuelve true' do
+        expect(piedra.le_gana_a? tijera ).to be_true
+    end
+end
+
