f3b6750c07a18f233078edec7237e934427d62b2
danwyry <danwyry@gmail.com>
2015-09-27 15:47:06 -0300

    Test de Jugada: empata_con? jugada_que_empata devuelve true si los elementos de ambas jugadas empatan

--- kata2/spec/jugada_spec.rb
+++ kata2/spec/jugada_spec.rb
@@ -5,6 +5,9 @@ require_relative '../model/elemento'
 
 describe 'Jugada' do
     let(:jugada) { Jugada.new(Jugador.new, Tijera) }
+    let(:jugada_que_empata) { Jugada.new(Jugador.new, Tijera) }
+    let(:jugada_que_pierde) { Jugada.new(Jugador.new, Papel) }
+    let(:jugada_que_gana) { Jugada.new(Jugador.new, Piedra) }
 
     it 'jugada se inicializa con un jugador' do
         expect(jugada.jugador).to be_a_kind_of Jugador
@@ -12,5 +15,9 @@ describe 'Jugada' do
 
     it 'jugada se inicializa con un elemento' do
         expect(jugada.elemento).to be_a_kind_of Elemento
+    end
+
+    it 'empata_con? jugada_que_empata devuelve true si los elementos de ambas jugadas empatan' do
+        expect(jugada.empata_con? jugada_que_empata).to be_truthy
     end
 end
