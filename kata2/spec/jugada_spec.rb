5d25b370b3de169a873ea845621c81cb15bd6a59
danwyry <danwyry@gmail.com>
2015-09-27 15:37:11 -0300

    Test de Jugada: jugada se inicializa con un elemento

--- kata2/spec/jugada_spec.rb
+++ kata2/spec/jugada_spec.rb
@@ -10,7 +10,7 @@ describe 'Jugada' do
         expect(jugada.jugador).to be_a_kind_of Jugador
     end
 
-#    it 'jugada se inicializa con un jugador' do
-#        expect(jugada.elemento).to  be
-#    end
+    it 'jugada se inicializa con un elemento' do
+        expect(jugada.elemento).to be_a_kind_of Elemento
+    end
 end
