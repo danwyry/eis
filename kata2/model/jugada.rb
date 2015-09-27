c459affafba08a57106c3a05740168fea86bb6c2
danwyry <danwyry@gmail.com>
2015-09-27 13:43:10 -0300

    Refactor: separación de las clases Elemento y sus subclases en un archivo aparte. No afecta la funcionalidad

--- kata2/model/jugada.rb
+++ kata2/model/jugada.rb
@@ -1,3 +1,4 @@
+require_relative 'elemento'
 class Jugada
 
     def initialize(jugador, tipo_elemento) 
@@ -21,44 +22,4 @@ class Jugada
         return @elemento
     end
 
-end
-
-class Elemento 
-
-    def initialize()
-        @les_gana_a = []
-    end
-
-    def empata_con?(otra_jugada)
-        return not( self.le_gana_a?(otra_jugada) or otra_jugada.le_gana_a?(self) )
-    end
-
-    def le_gana_a?(otra_jugada)
-        @les_gana_a.member? otra_jugada.class
-    end
-
-end
-
-class Tijera < Elemento
-    def initialize()
-        @les_gana_a = [ Papel , Mono ]
-    end
-end
-
-class Piedra < Elemento
-    def initialize()
-        @les_gana_a = [ Tijera ]
-    end
-end
-
-class Papel < Elemento
-    def initialize()
-        @les_gana_a = [ Piedra ]
-    end
-end
-
-class Mono < Elemento
-    def initialize()
-        @les_gana_a = [ Papel ]
-    end
 end
