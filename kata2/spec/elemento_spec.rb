5a04c88bf67638469de49f8b1b588c654eb13de9
danwyry <danwyry@gmail.com>
2015-09-27 14:31:48 -0300

    Test de Papel: le_gana_a? Tijera devuelve false

--- kata2/spec/elemento_spec.rb
+++ kata2/spec/elemento_spec.rb
@@ -88,3 +88,42 @@ describe 'Tijera' do
     end
 end
 
+describe 'Papel' do
+    let(:papel) { Papel.new() }
+    let(:tijera) { Tijera.new() }
+    let(:piedra) { Piedra.new() }
+    let(:mono) { Mono.new() }
+
+    it 'le_gana_a? Tijera devuelve false' do
+        expect(papel.le_gana_a? tijera ).to be_falsey
+    end
+
+#    it 'le_gana_a? Papel devuelve true' do
+#        expect(papel.le_gana_a? papel ).to be_truthy
+#    end
+#
+#    it 'le_gana_a? Mono devuelve true' do
+#        expect(papel.le_gana_a? mono).to be_truthy
+#    end
+#
+#    it 'le_gana_a? Piedra devuelve false' do
+#        expect(papel.le_gana_a? piedra).to be_falsey
+#    end
+#
+#    it 'empata_con? Tijera devuelve true' do
+#        expect(papel.empata_con? tijera).to be_truthy
+#    end
+#
+#    it 'empata_con? Papel devuelve false' do
+#        expect(papel.empata_con? papel).to be_falsey
+#    end
+#
+#    it 'empata_con? Piedra devuelve false' do
+#        expect(papel.empata_con? piedra).to be_falsey
+#    end
+#
+#    it 'empata_con? Mono devuelve false' do
+#        expect(papel.empata_con? mono).to be_falsey
+#    end
+end
+
