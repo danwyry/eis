bd3c7396267ac540bcf95d217c6aed469f33b1df
danwyry <danwyry@gmail.com>
2015-10-14 03:49:16 -0300

    Test de Naves: un submarino se inicializa sin averías

---
+++ kata3/spec/nave_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require_relative '../model/coordenada.rb'
+require_relative '../model/nave.rb'
+
+describe 'Submarino' do
+  let(:submarino) { Submarino.new }
+
+  it 'tamanio devuelve 1' do
+    expect(submarino.tamanio).to eq 1
+  end
+
+  it 'un submarino se inicializa sin averías' do 
+    expect(submarino.averiado?).to be_falsey
+  end
+end
+
+describe 'Crucero' do
+  let(:crucero) { Crucero.new }
+
+end
+
+describe 'Destructor' do
+  let(:destructor) { Destructor.new }
+end
