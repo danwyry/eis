90e4c227de9fdb1f99a519cbdf78898b9dc63820
danwyry <danwyry@gmail.com>
2015-10-14 03:34:13 -0300

    Test de Coordenada: siguiente(Horizontal, 1) devuelve una coordenada con valor x=2

--- kata3/spec/coordenada_spec.rb
+++ kata3/spec/coordenada_spec.rb
@@ -2,12 +2,16 @@ require 'rspec'
 require_relative '../model/coordenada.rb'
 
 describe 'Coordenada' do
-  let(:coord) { Coordenada.new(1,1) }
+  let(:coord) { Coordenada.new(1,2) }
 
   it 'coordenada se inicializa con los valores de x=1 e y=2' do
     expect( coord.x ).to eq 1
-    expect( coord.y ).to eq 1
+    expect( coord.y ).to eq 2
   end
   
+  it 'siguiente(Horizontal, 1) devuelve una coordenada con valor x=2' do
+    expect( coord.siguiente(Horizontal,1).x ).to eq 2
+  end
+
 end
  
