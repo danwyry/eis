3c53d940072211bb543eeac610929771ff16d597
danwyry <danwyry@gmail.com>
2015-10-23 03:39:12 -0300

    Test de Calculadora : al hacer un calculo cant_operaciones se incrementa en 1

--- kata4/features/step_definitions/memoria_steps.rb
+++ kata4/features/step_definitions/memoria_steps.rb
@@ -2,6 +2,21 @@ Given(/^no hice operaciones$/) do
   visit '/'
 end
 
+Given(/^hago una suma$/) do
+  visit '/'
+  fill_in 'operando', with: '1'
+  click_button 'otro_operando'
+  fill_in 'operando', with: '2'
+  click_button 'suma'
+end
+
+Given(/^hago una resta$/) do
+  fill_in 'operando', with: '4'
+  click_button 'otro_operando'
+  fill_in 'operando', with: '2'
+  click_button 'resta'
+end
+
 Then(/^la cantidad de operaiones es (\d+)$/) do | cantidad |
   with_scope('span#cant_operaciones') do
     expect(page.text).to eq cantidad.to_s
--- kata4/spec/calculadora_spec.rb
+++ kata4/spec/calculadora_spec.rb
@@ -7,8 +7,8 @@ describe 'Calculadora' do
   let (:calc) { Calculadora::Calculadora.new }
 
   it 'al inicializarse cant_operaciones es 0 ' do 
-    expect(calc.cant_operaciones).to eq 0 
-  end 
+    expect(calc.cant_operaciones).to eq 0
+  end
 
   it 'calcular Sumar, [1,2]' do
     expect(calc.calcular(Suma, [1,2]) ).to eq 3
@@ -16,6 +16,14 @@ describe 'Calculadora' do
 
   it 'calcular Restar, [2,1]' do
     expect(calc.calcular(Resta, [2,1]) ).to eq 1
+  end
+
+  it 'al hacer un calculo cant_operaciones se incrementa en 1' do
+    calc.calcular(Suma, [1,2])
+    expect(calc.cant_operaciones).to eq 1
+    calc.calcular(Suma, [1,2])
+    expect(calc.cant_operaciones).to eq 2
+
   end
 
 end
