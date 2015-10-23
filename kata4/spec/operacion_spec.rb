447197250243bc24eb1757f2c345b22d48dcbe0b
danwyry <danwyry@gmail.com>
2015-10-23 02:18:35 -0300

    Tests de Operacion:
    Suma
      si inicializo con [1] entonces operandos devuelve [1]
      si inicializo con [1] entonces resultado devuelve 1
    
    Resta
      si inicializo con [1,2] entonces operandos devuelve [1,2]
      si inicializo con [1] entonces resultado devuelve 1
    
    Promedio
      si inicializo con [1,2,6] entonces operandos devuelve [1,2,6]
      si inicializo con [1,2,6] entonces resultado devuelve 3

--- kata4/spec/operacion_spec.rb
+++ kata4/spec/operacion_spec.rb
@@ -4,7 +4,44 @@ require_relative '../app/models/operacion'
 describe 'Operacion' do
 
   it 'new([]) lanza una OperandosInsuficientesException' do
-
     expect{Operacion.new [] }.to raise_error OperandosInsuficientesException
+  end
+end
+
+describe 'Suma' do
+  let (:op) {Suma.new [1]}
+
+  it 'si inicializo con [1] entonces operandos devuelve [1] ' do
+    expect(op.operandos).to eq [1]
+  end
+
+  it 'si inicializo con [1] entonces resultado devuelve 1 ' do
+    expect(op.resultado).to eq 1
+  end
+
+
+end
+
+describe 'Resta' do
+  let (:op) {Resta.new [1,2]}
+
+  it 'si inicializo con [1,2] entonces operandos devuelve [1,2] ' do
+    expect(op.operandos).to eq [1,2]
+  end
+
+  it 'si inicializo con [1] entonces resultado devuelve 1 ' do
+    expect(op.resultado).to eq -1
+  end
+end
+
+describe 'Promedio' do
+  let (:op) {Promedio.new [1,2,6]}
+
+  it 'si inicializo con [1,2,6] entonces operandos devuelve [1,2,6] ' do
+    expect(op.operandos).to eq [1,2,6]
+  end
+
+  it 'si inicializo con [1,2,6] entonces resultado devuelve 3 ' do
+    expect(op.resultado).to eq 3
   end
 end
